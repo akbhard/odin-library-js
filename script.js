b69cb1ed99b43154b8a8d7b7b74c1160c7d29fd9
Akshay <akshay@Akshays-MacBook-Air.local>
2025-10-15 22:02:14 -0400

    Javascript file with initial functions to create a new book and add book to library

---
+++ script.js
@@ -0,0 +1,26 @@
+// I need a library that will store all my books. This will store the books as objects, not as their entire form.
+// It will be an array that I will push all books that are created into the library (array).
+
+const myLibrary = []
+
+// Now I need a way to create a book. The method of creating a book will be using constructors.
+// The book constructor will have to take in: author, title, number of pages, whether it’s been read.
+
+function Book(author, title, numberOfPages, read) {
+    this.uuid = crypto.randomUUID();
+    this.author = author;
+    this.title = title;
+    this.numberOfPages = numberOfPages;
+    this.read = read
+}
+
+// Now I need a function to take a newly created book and add it to my library.
+
+function addBookToLibrary (author, title, numberOfPages,read) {
+    const newBook = new Book (author, title, numberOfPages, read)
+    myLibrary.push(newBook)
+}
+
+// I now need a function that will take all the books in my library and display them in the HTML file.
+// Will most likely need to create a loop that takes the length of the array, iterate through each one.
+// For each index, it will create a page/display in the HTML. So will need to create a div in a table?
